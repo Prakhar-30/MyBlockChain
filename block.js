c534d9857eb10c60cbb89e9bc44154b702fa0835
PRakhar <srivastavaprakhar3010@gmail.com>
2024-06-09 13:46:15 +0530

    Created A hash and miner function

--- block.js
+++ block.js
@@ -1,4 +1,7 @@
 const {GENESIS_DATA}=require('./config');
+const cryptoHash = require('./crypto-hash');
+
+
 class block{
     constructor({timestamp,prevHash,hash,data}){
         this.timestamp=timestamp;
@@ -9,13 +12,27 @@ class block{
     static genesis(){
         return new this(GENESIS_DATA);
     }
+    static mineBlock({prevBlock,data}){
+        const timestamp=Date.now();
+        const prevHash=prevBlock.hash;
+        return new block({
+            timestamp,
+            prevHash,
+            data,
+            hash:cryptoHash(timestamp,prevHash,data),
+        });
+    }
 }
 
 const block1=new block({
     timestamp:'9/6/24',prevHash:'0xacb',hash:'0xc12',data:'hello'});
 const block2=new block({
     prevHash:'0xc12',timestamp:'10/6/24',hash:'0x123',data:'world'});
+
+
 const genesisBlock=block.genesis();
 console.log(genesisBlock);
-console.log(block1);
-console.log(block2);
+
+
+const result=block.mineBlock({prevBlock:block1,data:"block2"});
+console.log(result);
