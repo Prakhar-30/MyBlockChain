d65d304a98a58aafee1520fed7e36da21d0e4705
PRakhar <srivastavaprakhar3010@gmail.com>
2024-06-09 15:00:52 +0530

    Created a replaceChain function to replace with the longest valid chain

--- blockchain.js
+++ blockchain.js
@@ -13,21 +13,31 @@ class Blockchain {
     this.chain.push(newBlock);
   }
 
-  static isValidChain(chain){
-    if(JSON.stringify(chain[0])!==JSON.stringify(Block.genesis())){
-        return false;
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
     }
-    for(let i=1;i<chain.length;i++){
-        const{timestamp,prevHash,hash,data}=chain[i];
-        const realLastHash=chain[i-1].hash;
-        if(prevHash!==realLastHash)return false   ;
-        const validatedHash=cryptoHash(timestamp,prevHash,data);
-        if(hash!== validatedHash) return false;
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, prevHash, hash, data } = chain[i];
+      const realLastHash = chain[i - 1].hash;
+      if (prevHash !== realLastHash) return false;
+      const validatedHash = cryptoHash(timestamp, prevHash, data);
+      if (hash !== validatedHash) return false;
     }
     return true;
   }
 
-
+  replaceChain(chain) {
+    if (chain.length <= this.chain.length) {
+      console.error("The incomming chain is not longer");
+      return;
+    }
+    if (!Blockchain.isValidChain(chain)) {
+      console.error("the incoming chain is not valid");
+      return;
+    }
+    this.chain = chain;
+  }
 }
 
 const blockchain = new Blockchain();
