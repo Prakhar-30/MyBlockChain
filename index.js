4301ececc7cf608781835e3baa9b8c409e0e0634
PRakhar <srivastavaprakhar3010@gmail.com>
2024-06-09 23:21:23 +0530

    final commit

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
-const bodyParser=require('body-parser');
+const bodyParser = require("body-parser");
+const express = require("express");
+const Blockchain = require("./blockchain.js");
+const PubSub = require("./publishsubscribe.js");
+const request = require("request");
 
-const express=require('express');
+const app = express();
+const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
 
-const Blockchain=require('./blockchain.js');
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
-const app=express();
-
-const blockchain=new Blockchain();
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
-app.get('/api/blocks',(req,res)=>{
-    res.json(blockchain.chain)
-})
 
-app.post("/api/mine",(req,res)=>{
-    const {data}=req.body;
+app.get("/api/blocks", (req, res) => {
+  res.json(blockchain.chain);
+});
 
-    blockchain.addBlock({data});
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  pubsub.broadcastChain();
+  res.redirect("/api/blocks");
+});
 
-    res.redirect('/api/blocks')
-})
+const synChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        console.log("Replace chain on sync with", rootChain);
+        blockchain.replaceChain(rootChain);
+      } else {
+        console.error("Error syncing chains:", error);
+      }
+    }
+  );
+};
 
-const PORT=3000;
+let PEER_PORT;
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
 
-app.listen(PORT,()=>{
-    console.log(`listening to PORT:${PORT}`);
-})
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  if (PORT !== DEFAULT_PORT) {
+    synChains();
+  }
+});
