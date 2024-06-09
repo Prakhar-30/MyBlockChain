c564c707ac969c27ef8add80ca82bcd52187c763
PRakhar <srivastavaprakhar3010@gmail.com>
2024-06-09 14:38:09 +0530

    Created a blockchain structure that can add blocks

--- block.js
+++ block.js
@@ -1,38 +1,35 @@
-const {GENESIS_DATA}=require('./config');
+const { GENESIS_DATA } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
-
-class block{
-    constructor({timestamp,prevHash,hash,data}){
-        this.timestamp=timestamp;
-        this.prevHash=prevHash;
-        this.hash=hash;
-        this.data=data;
+class Block {
+    constructor({ timestamp, prevHash, hash, data }) {
+        this.timestamp = timestamp;
+        this.prevHash = prevHash;
+        this.hash = hash;
+        this.data = data;
     }
-    static genesis(){
+
+    static genesis() {
         return new this(GENESIS_DATA);
     }
-    static mineBlock({prevBlock,data}){
-        const timestamp=Date.now();
-        const prevHash=prevBlock.hash;
-        return new block({
+
+    static mineBlock({ prevBlock, data }) {
+        const timestamp = Date.now();
+        const prevHash = prevBlock.hash;
+        return new Block({
             timestamp,
             prevHash,
             data,
-            hash:cryptoHash(timestamp,prevHash,data),
+            hash: cryptoHash(timestamp, prevHash, data),
         });
     }
 }
 
-const block1=new block({
-    timestamp:'9/6/24',prevHash:'0xacb',hash:'0xc12',data:'hello'});
-const block2=new block({
-    prevHash:'0xc12',timestamp:'10/6/24',hash:'0x123',data:'world'});
+// Uncomment these to test genesis and mineBlock
+// const genesisBlock = Block.genesis();
+// console.log(genesisBlock);
 
+// const result = Block.mineBlock({ prevBlock: genesisBlock, data: "block2" });
+// console.log(result);
 
-const genesisBlock=block.genesis();
-console.log(genesisBlock);
-
-
-const result=block.mineBlock({prevBlock:block1,data:"block2"});
-console.log(result);
+module.exports = Block;
