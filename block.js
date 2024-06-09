30ffc9ae7c69404cbcfc5acdf5b88839fa3dedce
PRakhar <srivastavaprakhar3010@gmail.com>
2024-06-09 13:26:25 +0530

    created a genesis block

--- block.js
+++ block.js
@@ -1,13 +1,21 @@
+const {GENESIS_DATA}=require('./config');
 class block{
-    constructor(timestamp,prevHash,hash,data){
+    constructor({timestamp,prevHash,hash,data}){
         this.timestamp=timestamp;
         this.prevHash=prevHash;
         this.hash=hash;
         this.data=data;
     }
+    static genesis(){
+        return new this(GENESIS_DATA);
+    }
 }
 
-const block1=new block('9/6/24','0xacb','0xc12','hello');
-const block2=new block('10/6/24','0xc12','0x123','world');
+const block1=new block({
+    timestamp:'9/6/24',prevHash:'0xacb',hash:'0xc12',data:'hello'});
+const block2=new block({
+    prevHash:'0xc12',timestamp:'10/6/24',hash:'0x123',data:'world'});
+const genesisBlock=block.genesis();
+console.log(genesisBlock);
 console.log(block1);
 console.log(block2);
